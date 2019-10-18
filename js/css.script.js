2dd4db974e9735ad66fd85a17a00baa30488cfc5
Sasithon <sasie@live.com>
2019-10-18 11:48:58 +0200

    Validate
    Validation complete for HTML and CSS files, one image was compressed

--- js/css.script.js
+++ js/css.script.js
@@ -1,39 +1,39 @@
 function changeTextSize() {
 
 // Get HTML head element 
-	var head = document.getElementsByTagName('HEAD')[0];  
+var head = document.getElementsByTagName('HEAD')[0];  
 
-	// Create new link Element 
-	var link = document.createElement('link'); 
+// Create new link Element 
+var link = document.createElement('link'); 
 
-	// set the attributes for link element  
-	link.rel = 'stylesheet';  
-  
-	link.type = 'text/css'; 
-  
-	link.href = 'css/stil.text.script.css';  
+// set the attributes for link element  
+link.rel = 'stylesheet';  
 
-	// Append link element to HTML head 
-	head.appendChild(link);  
-	
+link.type = 'text/css'; 
+
+link.href = 'css/stil.text.script.css';  
+
+// Append link element to HTML head 
+head.appendChild(link);  
+
 }
 
 function changeBackground() {
 
 // Get HTML head element 
-	var head = document.getElementsByTagName('HEAD')[0];  
+var head = document.getElementsByTagName('HEAD')[0];  
 
-	// Create new link Element 
-	var link = document.createElement('link'); 
+// Create new link Element 
+var link = document.createElement('link'); 
 
-	// set the attributes for link element  
-	link.rel = 'stylesheet';  
-  
-	link.type = 'text/css'; 
-  
-	link.href = 'css/stil.bakgrund.script.css';  
+// set the attributes for link element  
+link.rel = 'stylesheet';  
 
-	// Append link element to HTML head 
-	head.appendChild(link);  
-	
+link.type = 'text/css'; 
+
+link.href = 'css/stil.bakgrund.script.css';  
+
+// Append link element to HTML head 
+head.appendChild(link);  
+
 }
